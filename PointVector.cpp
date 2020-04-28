41dff16548983b93d07516ccca848e58bcd05627
mwithoeft <moritz_pit.withoeft@fh-bielefeld.de>
2020-04-28 11:38:41 +0200

    Added PointVector Class

---
+++ PointVector.cpp
@@ -0,0 +1,127 @@
+#include "header/PointVector.h"
+using namespace std;
+
+
+//Konstruktoren
+PointVector::PointVector() {
+	this->xCoor = 0;
+	this->yCoor = 0;
+	this->zCoor = 0;
+	this->homoCoor = 0;
+}
+PointVector::PointVector(float x, float y, float z, float homo) {
+	this->xCoor = x;
+	this->yCoor = y;
+	this->zCoor = z;
+	this->homoCoor = homo;
+}
+PointVector::PointVector(glm::vec3& vec, float homo) {
+	this->xCoor = vec.x;
+	this->yCoor = vec.y;
+	this->zCoor = vec.z;
+	this->homoCoor = homo;
+}
+PointVector::PointVector(glm::vec4& vec) {
+	this->xCoor = vec.x;
+	this->yCoor = vec.y;
+	this->zCoor = vec.z;
+	this->homoCoor = vec.w;
+}
+
+//Copy-Konstruktor
+PointVector::PointVector(const PointVector& cp) {
+	this->xCoor = cp.xCoor;
+	this->yCoor = cp.yCoor;
+	this->zCoor = cp.zCoor;
+	this->homoCoor = cp.homoCoor;
+}
+
+//Zuweisungsoperator
+PointVector& PointVector::operator=(const PointVector& d) {
+	if (this != &d) {
+		this->xCoor = d.xCoor;
+		this->yCoor = d.yCoor;
+		this->zCoor = d.zCoor;
+		this->homoCoor = d.homoCoor;
+	}
+	return *this;
+}
+
+//Skalarmultiplikation
+PointVector operator*(PointVector& ptVector, float scalar) {
+	PointVector result;
+	result.xCoor = ptVector.xCoor * scalar;
+	result.yCoor = ptVector.xCoor * scalar;
+	result.zCoor = ptVector.xCoor * scalar;
+	result.homoCoor = ptVector.homoCoor * scalar;
+	if (result.homoCoor != 0 && result.homoCoor != 1) result.printHomoCoorWarning("Skalarmultiplikation");
+	return result;
+}
+PointVector operator*(float scalar, PointVector& ptVector) {
+	PointVector result;
+	result.xCoor = ptVector.xCoor * scalar;
+	result.yCoor = ptVector.xCoor * scalar;
+	result.zCoor = ptVector.xCoor * scalar;
+	result.homoCoor = ptVector.homoCoor * scalar;
+	if (result.homoCoor != 0 && result.homoCoor != 1) result.printHomoCoorWarning("Skalarmultiplikation");
+	return result;
+}
+
+//Subtraktion
+PointVector operator-(PointVector& ptVec1, PointVector& ptVec2) {
+	PointVector result;
+	result.xCoor = ptVec1.xCoor - ptVec2.xCoor;
+	result.yCoor = ptVec1.yCoor - ptVec2.yCoor;
+	result.zCoor = ptVec1.zCoor - ptVec2.zCoor;
+	result.homoCoor = ptVec1.homoCoor - ptVec2.homoCoor;
+	if (result.homoCoor != 0 && result.homoCoor != 1) result.printHomoCoorWarning("Subtraktion");
+	return result;
+}
+
+//Addition
+PointVector operator+(PointVector& ptVec1, PointVector& ptVec2) {
+	PointVector result;
+	result.xCoor = ptVec1.xCoor + ptVec2.xCoor;
+	result.yCoor = ptVec1.yCoor + ptVec2.yCoor;
+	result.zCoor = ptVec1.zCoor + ptVec2.zCoor;
+	result.homoCoor = ptVec1.homoCoor + ptVec2.homoCoor;
+	if (result.homoCoor != 0 && result.homoCoor != 1) result.printHomoCoorWarning("Addition");
+	return result;
+}
+
+//Skalarprodukt
+float operator*(PointVector& ptVec1, PointVector& ptVec2) {
+	float result;
+	result = (ptVec1.xCoor * ptVec2.xCoor) + (ptVec1.yCoor * ptVec2.yCoor) + (ptVec1.zCoor * ptVec2.zCoor);
+	return result;
+}
+
+//Kreuzprodukt
+PointVector PointVector::crossProduct(PointVector& ptVec2) {
+	PointVector result;
+	if (this->homoCoor != 0) result.printHomoCoorWarning("Kreuzprodukt Vektor 1");
+	if (ptVec2.homoCoor != 0) result.printHomoCoorWarning("Kreuzprodukt Vektor 2");
+	result.xCoor = (this->yCoor * ptVec2.zCoor) - (this->zCoor * ptVec2.yCoor);
+	result.yCoor = (this->zCoor * ptVec2.xCoor) - (this->xCoor * ptVec2.zCoor);
+	result.zCoor = (this->xCoor * ptVec2.yCoor) - (this->yCoor * ptVec2.xCoor);
+	result.homoCoor = 0;
+	return result;
+}
+
+//GLM Vektoren
+glm::vec3 PointVector::getVec3() {
+	return glm::vec3(this->xCoor, this->yCoor, this->zCoor);
+}
+glm::vec4 PointVector::getVec4() {
+	return glm::vec4(this->xCoor, this->yCoor, this->zCoor, this->homoCoor);
+}
+
+
+
+// Warnung wegen homogener Koordinaten
+void PointVector::printHomoCoorWarning(string methode) {
+	cout << "**********" << endl;
+	cout << "Methode: " << methode << endl;
+	cout << "Warnung bei homogener Komponenten(" << this->homoCoor << ") der Koordinate X:" << this->xCoor << " Y:" << this->yCoor << " Z:" << this->zCoor << endl;
+	cout << "**********" << endl;
+}
---
+++ header/PointVector.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "../include/GLSLProgram.h"
+
+using namespace std;
+
+class PointVector {
+
+public:
+	PointVector();												//0, 0, 0, 0
+	PointVector(float, float, float, float);					//X, Y, Z, HOMO
+	PointVector(glm::vec3&, float);								//VEC3, HOMO
+	PointVector(glm::vec4&);									//VEC4
+	PointVector(const PointVector&);							//Copy-Konstruktor
+	PointVector& operator=(const PointVector&);					//Zuweisungsoperator
+
+	friend PointVector operator*(PointVector&, float);			//Skalarmultiplikation
+	friend PointVector operator*(float, PointVector&);			//Skalarmultiplikation
+	friend PointVector operator-(PointVector&, PointVector&);	//Subtraktion
+	friend PointVector operator+(PointVector&, PointVector&);	//Addition
+	friend float operator*(PointVector&, PointVector&);			//Skalarprodukt
+	PointVector crossProduct(PointVector&);						//Kreuzprodukt
+
+	glm::vec3 getVec3();										//GLM Vec3
+	glm::vec4 getVec4();										//GLM Vec4
+
+	float xCoor;
+	float yCoor;
+	float zCoor;
+	float homoCoor;
+
+private:
+	void printHomoCoorWarning(string);
+};
